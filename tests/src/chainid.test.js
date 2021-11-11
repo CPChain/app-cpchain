a9af3ea508880da810eed2d726cceb24f1573a8e
liaojl <liaojl@cpchain.io>
2021-11-11 23:44:55 +0800

    first test

--- tests/src/chainid.test.js
+++ tests/src/chainid.test.js
@@ -9,12 +9,14 @@ test('[Nano S] Transfer on network 337 on CPChain', zemu("nanos", async (sim, et
 	 "f180808601a3185c5000830493e0941455d180e3ade94ebd9cc324d22a9065d1f5f575880de0b6b3a7640000808201518080"
   );
 
-  // await waitForAppScreen(sim);
-  // await sim.navigateAndCompareSnapshots('.', 'nanos_transfer_112233445566_network', [9, 0]);
+  await waitForAppScreen(sim);
 
-  // await expect(tx).resolves.toEqual({
-  //   "r": "509981d8dfb66757e25ff47c009b9b5bc5db0f169473e4735f5212b144f1c069",
-  //   "s": "5db989d81025de3c846e41a9ce01a3f9fd0982e2d827f1b88ffc95d73a48d04c",
-  //   "v": "344344f19f",
-  // });
+  await sim.navigateAndCompareSnapshots('.', 'nanos_transfer_112233445566_network', [6, 0]);
+
+
+  await expect(tx).resolves.toEqual({
+     "r": "fca632e24bf16fa399064962a71ae0157fc9ec04f6184c4c608b401a5e88c570",
+     "s": "0eb9aa3f5bf7ee4aacb0da87ae7be12430fd244dc24ee4ded07d68c2aa03a5ea",
+     "v": "c6",
+  });
 }));
